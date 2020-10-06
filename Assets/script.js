17cbccd5c203b2af6f785541e4cd4e8f2013a977
ziieng <cjengelhardt@gmail.com>
2020-10-06 15:28:58 -0700

    Initial push of structure and plans

---
+++ Assets/script.js
@@ -0,0 +1,25 @@
+// oh buddy it's the big times now
+
+//NEEDS
+// 1. text box to add a city
+// 2. list of previous cities requested
+//      a. way to keep most recent between pages
+//      b. way to remove a city
+// 3. current weather data for active city from list
+//      a. handling for if the city wasn't found?
+
+//TASKS
+// I. Accept city from text input
+//   a. check if valid city (test query OW?)
+//   b. store all cities requested
+// II. Display list of all cities used
+//   a. Populate sidebar list dynamically
+//   b. Buttons or just fields?
+//   c. Make current visibly different, store most recent used in storage too
+// III. Display current weather on city click
+//   a. query OW for current data
+//   b. parse and display: Temp in F, humidity, wind, UV index(, AQI?)
+// IV. Display 5-day forecast
+//   a. query OW for 5-day
+//   b. display on cards
+//      (check documentation, they may have a widget already for this)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # WeatherWidget
-A weather dashboard using the OpenWeather API
+Sixth homework assignment for uw-sea-fsf-pt-08-2020-u-c, [a weather dashboard using the OpenWeather API.](https://github.com/ziieng/WeatherWidget)
